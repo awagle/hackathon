1fbb92fc37df6635c2d31ff2cc2e8e64480fdde5
Aditya Wagle <awagle@tibco.com>
2016-08-13 11:55:59 -0700

    Adding UI binding manager code and schemas for Input and Output

--- jira/com.tibco.bw.palette.jira.design/src/com/tibco/bw/palette/jira/design/query/JIRAQueryGeneralPropertySection.java
+++ jira/com.tibco.bw.palette.jira.design/src/com/tibco/bw/palette/jira/design/query/JIRAQueryGeneralPropertySection.java
@@ -3,15 +3,20 @@ package com.tibco.bw.palette.jira.design.query;
 import javax.xml.namespace.QName;
 
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 
 import com.tibco.bw.design.field.BWFieldFactory;
+import com.tibco.bw.design.field.PropertyField;
 import com.tibco.bw.design.propertysection.AbstractBWTransactionalSection;
 import com.tibco.bw.design.util.BWDesignConstants;
+import com.tibco.bw.palette.jira.model.jiraPalette.JiraPalettePackage;
 import com.tibco.bw.palette.jira.model.jiraPalette.Query;
 
 public class JIRAQueryGeneralPropertySection extends
 AbstractBWTransactionalSection {
 	QName HTTP_QNAME = new QName("{http://xsd.tns.tibco.com/bw/models/sharedresource/httpclient}", "HttpClientConfiguration");
+	private PropertyField propField;
+	private Text textField;
 
 	public JIRAQueryGeneralPropertySection() {
 		// TODO Auto-generated constructor stub
@@ -20,6 +25,10 @@ AbstractBWTransactionalSection {
 	@Override
 	protected void initBindings() {
 		// TODO Auto-generated method stub
+		getBindingManager().bind(propField, JiraPalettePackage.Literals.QUERY__CONNECTION, getInput(), 
+				BWFieldFactory.getInstance().getPropertyTargetToModelUpdateValueStrategy(), null);
+
+		getBindingManager().bind(textField, getInput(), JiraPalettePackage.Literals.QUERY__EXPAND_NAMES);
 
 	}
 
@@ -28,10 +37,11 @@ AbstractBWTransactionalSection {
 		// TODO Auto-generated method stub
 		Composite parent = BWFieldFactory.getInstance().createComposite(root, 2);
 		BWFieldFactory.getInstance().createLabel(parent, "Connection", true);
-		BWFieldFactory.getInstance().createPropertyField(parent, BWDesignConstants.PROPERTY, HTTP_QNAME);
+		propField = BWFieldFactory.getInstance().createPropertyField(parent, BWDesignConstants.PROPERTY, HTTP_QNAME);
+		propField.setDefaultPropertyPrefix("jira");
 
 		BWFieldFactory.getInstance().createLabel(parent, "OutputFields", true);
-		BWFieldFactory.getInstance().createTextBox(parent);
+		textField = BWFieldFactory.getInstance().createTextBox(parent);
 		return parent;
 	}
 
--- jira/com.tibco.bw.palette.jira.design/src/com/tibco/bw/palette/jira/design/query/JIRAQuerySignature.java
+++ jira/com.tibco.bw.palette.jira.design/src/com/tibco/bw/palette/jira/design/query/JIRAQuerySignature.java
@@ -2,13 +2,19 @@ package com.tibco.bw.palette.jira.design.query;
 
 import java.util.List;
 
+import org.eclipse.xsd.XSDCompositor;
 import org.eclipse.xsd.XSDElementDeclaration;
+import org.eclipse.xsd.XSDModelGroup;
+import org.eclipse.xsd.XSDSchema;
 
 import com.tibco.bw.design.api.BWActivitySignature;
 import com.tibco.bw.design.api.BWActivitySignatureUnknown;
+import com.tibco.bw.design.util.XSDUtility;
 import com.tibco.bw.model.activityconfig.Configuration;
 
 public class JIRAQuerySignature extends BWActivitySignature {
+
+	protected final static String TARGET_NS = "http://www.tibco.com/namespaces/tnt/plugins/jira"; 
 
 	public JIRAQuerySignature() {
 		// TODO Auto-generated constructor stub
@@ -24,8 +30,18 @@ public class JIRAQuerySignature extends BWActivitySignature {
 	@Override
 	public XSDElementDeclaration getInputType(Configuration config)
 			throws BWActivitySignatureUnknown {
-		// TODO Auto-generated method stub
-		return null;
+		String namespace = createNamespace(new Object[] { TARGET_NS, config,
+		"input" });
+		XSDSchema schema = XSDUtility.createSchema(namespace);
+		XSDElementDeclaration retVal = null;
+		XSDModelGroup inputGroup = XSDUtility.addAnonymousComplexTypeElement(schema, getInputTypeRootName(), XSDCompositor.SEQUENCE_LITERAL);
+
+		XSDUtility.addSimpleTypeElement(inputGroup, "jql", "string", 1, 1);
+		XSDUtility.addSimpleTypeElement(inputGroup, "timout", "long", 0, 1);
+		XSDUtility.addSimpleTypeElement(inputGroup, "limit", "long", 0, 1);
+		schema = compileSchema(schema);
+		retVal = schema.resolveElementDeclaration(getInputTypeRootName());
+		return retVal;
 	}
 
 	@Override
@@ -33,6 +49,22 @@ public class JIRAQuerySignature extends BWActivitySignature {
 			throws BWActivitySignatureUnknown {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public String getInputTypeRootName() {
+		return "jiraQueryActivityInput"; //$NON-NLS-1$
+	}
+
+	@Override
+	public String getOutputTypeRootName() {
+		return "jiraQueryOutput"; //$NON-NLS-1$
+	}
+
+
+	@Override
+	public boolean isSynchronous() {
+		return false;
 	}
 
 }
